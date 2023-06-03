c7fc7ceb405712fae62079060ac4ca06785bf389
s.leontev <neonyan15@gmail.com>
2023-06-03 13:58:10 +0300

    add functions for program

---
+++ src/functions.py
@@ -0,0 +1,34 @@
+def read_json():
+    """
+    Читает файл json и возвращает его содержимое
+    :return: Данные в виде списка словарей
+    """
+    pass
+
+
+def sort_data(data):
+    """
+    сортирует данные по статусу операции
+    :param data: исходные даннеы для сортировки
+    :return: Отсортированные данные
+    """
+    pass
+
+
+def mask_card_number(card_number):
+    """
+    Заменяет нужные цифры у номера карты на *
+    :param card_number: Исходный номер карты
+    :return: Маскированную строку
+    """
+    pass
+
+
+def mask_amount_number(amount_number):
+    """
+    Заменяет нужные цифры у номера счета на *
+    :param amount_number: Исходный номер счета
+    :return: Маскированную строку
+    """
+    pass
+
